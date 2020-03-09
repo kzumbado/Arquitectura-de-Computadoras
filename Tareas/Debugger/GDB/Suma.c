408ea18f912492ee6fc258dd905ad0ea7b888408
Kevin Z <kevin.zumbadocruz@gmail.com>
2020-03-08 18:25:40 -0600

    Nuevo contenido

---
+++ Tareas/Debugger/GDB/Suma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int x, y= 0;
+
+    printf("Digite el primer numero: ");
+    scanf("%d",&x);
+
+    printf("Digite el segundo numero: ");
+    scanf("%d",&y);
+
+    printf("La suma es %d \n", x+y); 
+
+    return 0;
+}
